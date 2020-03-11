d7078d110587414ee76b22b5ea13585df3ac34d3
duprezf <Florian.duprez@student.uclouvain.be>
2020-03-11 23:58:22 +0100

    test product, all, any

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -168,6 +168,55 @@ void test_v_drop(){
     static_assert(vlists::sum<L8>::value == 0);
 }
 
+template<auto T>
+struct pair
+{
+    static constexpr bool value = T % 2 = 0;
+};
+
+
+void test_v_all(){
+    using L1 = vlists::List<1,2,3,4,5,6,7,8>;
+    using L2 = vlists::List<2,4,6,8,10>;
+    using L3 = vlists::List<>;
+    using L4 = vlists::List<-1,-3,-5>;
+
+    static_assert(vlists::all<L1,pair>::value == false);
+    static_assert(vlists::all<L2,pair>::value == true);
+    static_assert(vlists::all<L3,pair>::value == true);
+    static_assert(vlists::all<L4,pair>::value == false);
+
+}
+void test_v_any(){
+    using L1 = vlists::List<1,2,3,4,5,6,7,8>;
+    using L2 = vlists::List<2,4,6,8,10>;
+    using L3 = vlists::List<>;
+    using L4 = vlists::List<-1,-3,-5>;
+
+    static_assert(vlists::any<L1,pair>::value == true);
+    static_assert(vlists::any<L2,pair>::value == true);
+    static_assert(vlists::any<L3,pair>::value == true);
+    static_assert(vlists::any<L4,pair>::value == false);
+
+}
+void test_v_product(){
+    using L1 = vlists::List<1,2,3,4,5>;
+    using L2 = vlists::List<0,63,29,18,18>;
+    using L3 = vlists::List<>;
+    using L4 = vlists::List<-1,1,2,3,4,5>;
+
+    static_assert(vlists::product<L1>::value == 120);
+    static_assert(vlists::product<L2>::value == 0);
+    static_assert(vlists::product<L3>::value == 1);
+    static_assert(vlists::product<L4>::value == -120);
+}
+void test_v_foldr(){
+
+}
+void test_v_foldr1(){
+
+}
+
 int main() {
     test_v_and();
     test_v_or();
@@ -179,6 +228,11 @@ int main() {
     test_v_min();
     test_v_max();
     test_v_drop();
+    test_v_all();
+    test_v_any();
+    test_v_product();
+    test_v_foldr();
+    test_v_foldr1();
 
     cout << "Everything is fine" <<endl;
 return 0;
